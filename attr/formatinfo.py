af0322277cfaf2e4bb27c561fa607d35ea2dd0af
James Murphy <james.f.murphy.one@gmail.com>
2018-05-27 00:59:16 -0400

    moving directories with git can be a pain sometimes

---
+++ attr/formatinfo.py
@@ -0,0 +1,17 @@
+def wordcase(s):
+  return ' '.join([word.capitalize() for word in s.split(' ')])
+
+skill_tag = 's_'
+perk_tag  = 'p_'
+
+def digest(cls, scope_dict):
+  for key in scope_dict:
+    if key.startswith(skill_tag):
+      skill = key.replace(skill_tag, '')
+      skill = wordcase(skill.replace('_',' '))
+      cls.skills[skill] = (cls.name, scope_dict[key])
+    elif key.startswith(perk_tag):
+      perk = key.replace(perk_tag, '')
+      perk = wordcase(perk.replace('_', ' '))
+      cls.perks[perk] = (cls.name, scope_dict[key])
+  
