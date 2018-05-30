aefa8dffe8028aee1f0b29cc7ada524eda5f2f83
James Murphy <james.f.murphy.one@gmail.com>
2018-05-29 23:45:25 -0400

    change optional magic attributes

---
+++ opt/Anima.py
@@ -0,0 +1,20 @@
+from dice_kernel import skill_check
+from formatinfo import digest
+class Anima:
+  '''Anima namespace. Do not edit the class. Instead, add skills
+  and perks below. Prefix skill names with "s_" and perk names with
+  "p_". Do not remove the call to "digest" from the bottom of the
+  file.'''
+  name   = 'Anima'
+  skills = { }
+  perks  = { }
+
+# Add your skills and perks here
+def s_example(anima, proficiency, dc):
+  '''Example skill check.'''
+  return skill_check(anima, proficiency, dc)
+
+# Fill the namespace without needing to type skill names and descriptions
+# into a crowded dict. Do not remove this call.
+digest(Anima, globals())
+
--- opt/Corpus.py
+++ opt/Corpus.py
@@ -1,20 +1,20 @@
 from dice_kernel import skill_check
 from formatinfo import digest
-class Faith:
-  '''Faith namespace. Do not edit the class. Instead, add skills
+class Corpus:
+  '''Corpus namespace. Do not edit the class. Instead, add skills
   and perks below. Prefix skill names with "s_" and perk names with
   "p_". Do not remove the call to "digest" from the bottom of the
   file.'''
-  name   = 'Faith'
+  name   = 'Corpus'
   skills = { }
   perks  = { }
 
 # Add your skills and perks here
-def s_example(faith, proficiency, dc):
+def s_example(corpus, proficiency, dc):
   '''Example skill check.'''
-  return skill_check(faith, proficiency, dc)
+  return skill_check(corpus, proficiency, dc)
 
 # Fill the namespace without needing to type skill names and descriptions
 # into a crowded dict. Do not remove this call.
-digest(Faith, globals())
+digest(Corpus, globals())
 
