fe4563e4682ee9296a06350a59178e4671bef14c
James Murphy <james.f.murphy.one@gmail.com>
2018-07-02 21:02:21 -0400

    add schools of magic to Corpus

--- opt/Corpus.py
+++ opt/Corpus.py
@@ -18,15 +18,27 @@ def s_restoration(corpus, proficiency, dc):
   return skill_check(corpus, proficiency, dc)
 
 def s_destruction(corpus, proficiency, dc):
-  '''Destruction is a school of magic which involves...'''
+  '''Destruction is a school of magic which involves the disintegration of
+  matter, the sapping of resources from organic lifeforms, and chain
+  reactions. Igniting fires and inducing magical illness are some abilities
+  that stem from the school of Destruction. Perks or spells that call for
+  Destruction will require a skill check with this skill.'''
   return skill_check(corpus, proficiency, dc)
 
 def s_augmentation(corpus, proficiency, dc):
-  '''Augmentation is a school of magic which involves...'''
+  '''Augmentation is a school of magic which involves bolstering physical
+  size and strength, protecting from or preventing damage, and fine-tuning
+  the senses. With Augmentation, a caster might enlarge stick to make a
+  better lever, or focus a creature's hearing on distant sounds. Perks or
+  spells which call for Augmentation will require this skill check.'''
   return skill_check(corpus, proficiency, dc)
 
 def s_diminution(corpus, proficiency, dc):
-  '''Diminution is a school of magic which involves...'''
+  '''Diminution is a school of magic which involves reducing physical size
+  and strength, revealing or increasing the weaknesses of creatures and
+  objects, and obscuring the senses. With Diminution, a caster might deafen
+  an animal to sneak up on it, or shrink a stone to skip across water. Perks
+  or skills which call for Diminution will require this skill check.'''
   return skill_check(corpus, proficiency, dc)
 
  
