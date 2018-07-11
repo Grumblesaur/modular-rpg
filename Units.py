9ee0848cae65721d6b83841180c80571a873205e
James Murphy <james.f.murphy.one@gmail.com>
2018-07-10 22:58:06 -0400

    spell sketches

---
+++ Units.py
@@ -0,0 +1,12 @@
+def tiles_to_feet(tiles):
+  '''One "tile" is five feet across, and costs as much
+  movement to traverse.'''
+  return tiles * 5
+
+def feet_to_tiles(feet):
+  return feet / 5
+
+
+
+
+
